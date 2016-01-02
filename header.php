67c7a0e97380518c102dbcc686d55fbde8d26512
Daniel Ehniss <daniel@depone.de>
2016-01-03 00:13:23 +0100

    Optimize title and description

--- header.php
+++ header.php
@@ -14,14 +14,14 @@
 
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
 
-<title><?php wp_title(' &middot;', true, 'right'); ?> <?php bloginfo('name'); ?> &middot; <?php bloginfo('description'); ?></title>
+<title><?php wp_title(' &middot;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 
 <meta name="author" content="Daniel Ehniss" />
 <meta name="keywords" content="Blog, Daniel Ehniss, Ehniss, Karlsruhe, Web2.0, Web, html, html5, responsive webdesign, responsive, css3, Mail, XHTML, CSS, Webstandards, Wordpress, Netzgestaltung, Webdesign, Web Design, Depone" />
 <?php if (is_single()) { ?>
     <meta name="description" content="<?php echo get_the_excerpt(); ?>" />
 <?php } else { ?>
-    <meta name="description" content="Hallo, ich bin Daniel Ehniss, ein Web Designer in Karlsruhe. DEPONE Netzgestaltung ist der Rahmen in dem ich Webseiten entwickle. Webseiten mit Leidenschaft, klar und kommunikativ." >
+    <meta name="description" content="<?php bloginfo('description'); ?>" >
 <?php } ?>
 
 <link href="<?php echo esc_url( home_url( '/favicon.ico' ) ); ?>" rel="shortcut icon" type="image/x-icon">
