2a499e8f31bac8b269a320eb5339b98b05965d65
Daniel Ehniss <daniel@depone.de>
2016-01-02 19:56:13 +0100

    Erase the maximum-scale from viewport-meta-tag

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 <head>
 <meta charset="<?php bloginfo('charset'); ?>" >
 
-<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
 
 <title><?php wp_title(' &middot;', true, 'right'); ?> <?php bloginfo('name'); ?> &middot; <?php bloginfo('description'); ?></title>
 
