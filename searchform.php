24814512caa81b5e5827f513af12cc9841219278
Daniel Ehniss <daniel@depone.de>
2017-12-05 17:50:04 +0100

    Refactor for better a11y

--- searchform.php
+++ searchform.php
@@ -1,4 +1,5 @@
 <form method="get" class="suchformular" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-		<input type="text" name="s" class="suchfeld" placeholder="<?php esc_attr_e( '...', '' ); ?>" />
-		<input type="submit" name="submit" class="suchen" value="<?php esc_attr_e( 'suchen', '' ); ?>" />
+	<label class="visually-hidden" for="searchterm">Webseite durchsuchen:</label>
+	<input id="searchterm" type="text" name="s" class="suchfeld" placeholder="<?php esc_attr_e( '...', '' ); ?>" />
+	<input type="submit" name="submit" class="suchen" value="<?php esc_attr_e( 'suchen', '' ); ?>" />
 </form>
