2942fb500752399926a37b883d0d9220edbbcbf3
Daniel Ehniss <daniel@depone.de>
2018-05-11 15:21:11 +0200

    Kill the referrer header entirely for all requests

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 
 <head>
 <meta charset="<?php bloginfo('charset'); ?>">
-
+<meta name="referrer" content="no-referrer">
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
 
 <title><?php wp_title(' &middot;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
