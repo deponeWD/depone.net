59a36855f04c2a5d60b61922418978344e76c8b5
Daniel Ehniss <daniel@depone.de>
2019-07-11 11:47:31 +0200

    Remove Büro5&30 from footer

--- footer.php
+++ footer.php
@@ -1,15 +1,9 @@
 <footer role="contentinfo">
-    <ul class="first">
+    <ul class="first first--centered">
         <li><strong>DEPONE Netzgestaltung</strong></li>
         <li>Daniel Ehniss</li>
         <li>daniel@depone.de</li>
         <li><a rel="me" class="url" href="https://twitter.com/depone" title="Link zu meinem Twitterprofil" target="_blank" >@depone</a></li>
-    </ul>
-    <ul class="second" >
-        <li><strong>B&uuml;ro 5&amp;30</strong></li>
-        <li>Gottesauer Straße 35</li>
-        <li>76131 Karlsruhe</li>
-        <li><a href="https://5und30.de" title="Link zur Webseite des Büro 5&30" >5und30.de</a></li>
     </ul>
     <ul class="third" >
         <li><a href="<?php echo esc_url( home_url( '/kontakt/' ) ); ?>" title="Link zur Kontaktseite" >Kontakt</a></li>
