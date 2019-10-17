687e6ea0b85e05e6efe685da130e645a62695bd0
Daniel Ehniss <daniel@depone.de>
2019-10-17 11:15:50 +0200

    Add filter for images in dark mode and meta-tag informing browsers about compatibility

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
 <meta charset="<?php bloginfo('charset'); ?>">
 <meta name="referrer" content="no-referrer">
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+<meta name="supported-color-schemes" content="light dark">
 
 <title><?php wp_title(' &middot;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 
