c5b749f5363e0f42e59af42df391814ec0a41aec
Daniel Ehniss <daniel@depone.de>
2020-03-06 17:43:07 +0100

    Update serviceworker

--- assets/js/serviceworker.js
+++ assets/js/serviceworker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const version = 'v0.09::';
+const version = 'v0.10::';
 const staticCacheName = version + 'static';
 const pagesCacheName = 'pages';
 const imagesCacheName = 'images';
