d00c328c835ad5a0df1cdab8cc6218ec50c94374
Daniel Ehniss <daniel@depone.de>
2020-08-15 17:52:17 +0200

    Remove *.woff from serviceworker

--- functions.php
+++ functions.php
@@ -2,7 +2,17 @@
 /**
  * Set Version Number
 */
-  define( 'CURRENT_THEME_VERSION', '2.0.1');
+  define( 'CURRENT_THEME_VERSION', '2.0.2');
+/**
+ * Disable Gutenberg editor.
+*/
+  add_filter('use_block_editor_for_post_type', '__return_false', 10);
+  // Don't load Gutenberg-related stylesheets.
+  add_action( 'wp_enqueue_scripts', 'remove_block_css', 100 );
+  function remove_block_css() {
+    wp_dequeue_style( 'wp-block-library' ); // Wordpress core
+    wp_dequeue_style( 'wp-block-library-theme' ); // Wordpress core
+  }
 /**
     * 2 Seitenleisten wird eingerichtet
 */
