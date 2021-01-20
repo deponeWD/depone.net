f1dd17ead05992c7f1b261e461fef204ca3e28fc
Daniel Ehniss <daniel@depone.de>
2021-01-20 20:02:23 +0100

    Remove API protection

--- functions.php
+++ functions.php
@@ -233,15 +233,4 @@ add_filter('comment_form_default_fields','my_fields');
   else :
     automatic_feed_links();
   endif;
-
-  // Protect Rest API
-  add_filter( 'rest_authentication_errors', function( $result ) { 
-    if ( ! empty( $result ) ) { 
-      return $result; 
-    } 
-    if ( ! is_user_logged_in() ) { 
-      return new WP_Error( '401', 'not allowed.', array('status' => 401) ); 
-    } 
-    return $result;
-  });
 ?>
